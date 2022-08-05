d3a45e044379a50daf29401c52970f7d6a926cbb
misael-reyes <misaelstark11@gmail.com>
2022-08-05 12:48:33 -0500

    register and login with email and password

--- app/src/main/java/com/example/misachat/iu/chat/ChatActivity.kt
+++ app/src/main/java/com/example/misachat/iu/chat/ChatActivity.kt
@@ -2,11 +2,34 @@ package com.example.misachat.iu.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.example.misachat.R
+import com.google.firebase.auth.FirebaseAuth
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+    }
+
+    // esto es para inflar el menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    // gestionamos los eventos de los clic del menj
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_logout -> {
+                // cerramos sesion
+                FirebaseAuth.getInstance().signOut()
+                onBackPressed() // regresamos a la ventana anterior
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/misachat/iu/login/LoginActivity.kt
+++ app/src/main/java/com/example/misachat/iu/login/LoginActivity.kt
@@ -1,12 +1,82 @@
 package com.example.misachat.iu.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.misachat.R
+import com.example.misachat.databinding.ActivityLoginBinding
+import com.example.misachat.iu.chat.ChatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    // binding
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autenticación"
+        binding.btnRegister.setOnClickListener {
+            // por el momento solo validamos que los textos no esten vacios
+            if (binding.textInputEmail.editText?.text!!.isNotEmpty() && binding.textInputPassword.editText?.text!!.isNotEmpty()) {
+                // podemos registrar a nuestro usuario
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(binding.textInputEmail.editText?.text.toString(),
+                        binding.textInputPassword.editText?.text.toString()).addOnCompleteListener {
+                            // para saber si se pudo registrar o hubo un error
+                            if (it.isSuccessful) {
+                                // nos vamos a la sala del chat
+                                    // si queremos acceder a el email por ejejmplo, hacemos esto
+                                        //it.result?.user?.email ?: ""
+                                showChat()
+                            } else {
+                                Toast.makeText(this, "No se pudo registrar", Toast.LENGTH_SHORT).show()
+                            }
+                    }
+            } else {
+                binding.textInputEmail.error = "pendejo, rellenalo"
+                binding.textInputPassword.error = "pendejo, rellenalo"
+            }
+        }
+
+        binding.btnLogin.setOnClickListener {
+            if (binding.textInputEmail.editText?.text!!.isNotEmpty() && binding.textInputPassword.editText?.text!!.isNotEmpty()) {
+                // accedemos
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(binding.textInputEmail.editText?.text.toString(),
+                        binding.textInputPassword.editText?.text.toString()).addOnCompleteListener {
+                        // para saber si se pudo acceder o hubo un error
+                        if (it.isSuccessful) {
+                            // nos vamos a la sala del chat
+                            // si queremos acceder a el email por ejemplo, hacemos esto
+                            //it.result?.user?.email ?: ""
+                            showChat()
+                        } else {
+                            Toast.makeText(this, "Credenciales invalidas", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                binding.textInputEmail.error = "pendejo, rellenalo"
+                binding.textInputPassword.error = "pendejo, rellenalo"
+            }
+        }
+    }
+
+    private fun showChat() {
+        val intent = Intent(this, ChatActivity::class.java).apply {
+            // podremos hacer esto para pasar parametros a la nueva activity
+            putExtra("", "")
+        }
+        startActivity(intent)
+        finish()
     }
 }
